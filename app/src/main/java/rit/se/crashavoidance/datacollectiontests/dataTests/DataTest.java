a5effdc7fe576f864c2a533b8541bc5e1050b7c8
Brett Tuttle <tuttle.brett@gmail.com>
2016-06-30 15:21:05 -0400

    Added more data, switched to using deferred objects for tests

--- app/src/main/java/rit/se/crashavoidance/datacollectiontests/dataTests/DataTest.java
+++ app/src/main/java/rit/se/crashavoidance/datacollectiontests/dataTests/DataTest.java
@@ -1,10 +1,14 @@
 package rit.se.crashavoidance.datacollectiontests.dataTests;
 
+import org.jdeferred.impl.DeferredObject;
+
 /**
  * Created by Dan on 6/28/2016.
  */
 public interface DataTest {
 
     public void run();
+
+    public DeferredObject getDeferredObject();
 
 }
--- app/src/main/java/rit/se/crashavoidance/datacollectiontests/dataTests/DiscoverServicesTest.java
+++ app/src/main/java/rit/se/crashavoidance/datacollectiontests/dataTests/DiscoverServicesTest.java
@@ -10,6 +10,9 @@ import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import org.jdeferred.Deferred;
+import org.jdeferred.impl.DeferredObject;
+
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
@@ -22,6 +25,42 @@ import rit.se.crashavoidance.datacollectiontests.service.DBParcelable;
  * Created by Dan on 6/28/2016.
  */
 public class DiscoverServicesTest implements DataTest {
+
+    WifiDirectHandler wifiDirectHandler;
+    Context context;
+    final String stepName = "DiscoverService";
+    long startTime;
+    long endTime;
+    boolean serviceBound = false;
+    private DeferredObject deferredObject;
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        // We only need to listen for one intent
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // We only need to listen for one intent
+            endTime = new Date().getTime();
+            wifiDirectHandler.stopDiscoveringServices();
+            Log.i("Tester", "Service discovered");
+            DBParcelable parcelable = new DBParcelable(stepName, startTime, endTime);
+            Intent i = new Intent();
+            i.setComponent(new ComponentName("rit.se.crashavoidance.datacollector", "rit.se.crashavoidance.datacollector.DBHandlerService"));
+            i.putExtra("record", parcelable);
+            ComponentName c = context.startService(i);
+            Log.i("Tester", parcelable.toString());
+            if(serviceBound) {
+                try {
+                    context.unbindService(wifiConnection);
+                }catch (Exception e){
+                    //TODO: cause of this exception?
+                    Log.e("TAG", "Caught exception", e);
+                }
+            }
+            LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
+            deferredObject.resolve("Completed Test");
+        }
+    };
+
     private ServiceConnection wifiConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -41,54 +80,25 @@ public class DiscoverServicesTest implements DataTest {
         }
     };
 
-    WifiDirectHandler wifiDirectHandler;
-    BroadcastReceiver receiver;
-    Context context;
-    final String stepName = "DiscoverService";
-    long startTime;
-    long endTime;
-
-    boolean serviceBound = false;
-
     public DiscoverServicesTest(Context context){
         this.context = context;
+        this.deferredObject = new DeferredObject();
         Log.i("Tester", "Instantiating");
     }
 
     public void run() {
-        Log.i("Tester", "starting the test");
         IntentFilter filter = new IntentFilter();
         filter.addAction(WifiDirectHandler.Action.DNS_SD_SERVICE_AVAILABLE);
-        receiver = new BroadcastReceiver() {
-            // We only need to listen for one intent
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                // We only need to listen for one intent
-                endTime = new Date().getTime();
-                wifiDirectHandler.stopDiscoveringServices();
-                Log.i("Tester", "Service discovered");
-                DBParcelable parcelable = new DBParcelable(stepName, startTime, endTime);
-                Intent i = new Intent();
-                i.setComponent(new ComponentName("rit.se.crashavoidance.datacollector", "rit.se.crashavoidance.datacollector.DBHandlerService"));
-                i.putExtra("record", parcelable);
-                ComponentName c = context.startService(i);
-                Log.i("Tester", parcelable.toString());
-                if(serviceBound) {
-                try {
-                    context.unbindService(wifiConnection);
-                }catch (Exception e){
-                    //TODO: cause of this exception?
-                    Log.e("TAG", "Caught exception", e);
-                }
-                }
-                LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
-            }
-        };
         LocalBroadcastManager.getInstance(context).registerReceiver(receiver, filter);
         /* This binds to the service, which calls continuouslyDiscoverServices
            It needs to be done this way because the call is asynchronous so the logic needs
            to be in the callback */
         Log.i("Tester", "Binding to the service");
         context.bindService(new Intent(context, WifiDirectHandler.class), wifiConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public DeferredObject getDeferredObject() {
+        return deferredObject;
     }
 }
